1668c398de2f69c302ab95fb3f48c00bd55169c4
Ehsan Bikdeli <ebikdeli@gmail.com>
2025-04-23 22:03:36 +0330

    multiprocessing added but not tested

---
+++ tests/test_multiprosessing.py
@@ -0,0 +1,21 @@
+import unittest
+from multiprocessing import Pool
+import random
+import time
+
+
+class MultiProcessingTest(unittest.TestCase):
+    """Test multiprocessing"""
+    def setUp(self):
+        return super().setUp()
+    
+    def _intensive_process(name: str, wait_time: int):
+        print(process_)
+    
+    def test_pool(self):
+        """Test multiprocessor using pool"""
+        with Pool(10) as p:
+            
+    
+    def tearDown(self):
+        return super().tearDown()
